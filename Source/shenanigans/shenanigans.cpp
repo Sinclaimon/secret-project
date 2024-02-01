c4b53be65ca98a664b28e68377a51674b461dbbc
Simon Matousek <sm255559@falmouth.ac.uk>
2024-02-01 15:47:22 +0000

    init commit

---
+++ Source/shenanigans/shenanigans.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "shenanigans.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, shenanigans, "shenanigans" );
